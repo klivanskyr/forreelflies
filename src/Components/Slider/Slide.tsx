bfea9a39d53c396ad2ee6ce279e4e17053f7ad4d
klivanskyr <klivanskyr@gmail.com>
2024-11-05 21:45:50 -0500

    testing images

--- src/Components/Slider/Slide.tsx
+++ src/Components/Slider/Slide.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Slide({ className="", backgroundSrc, children }: { className?: string, backgroundSrc?: string, children: React.ReactNode }) {
     return (
         <div className={`${className} relative w-full h-full`}>
-            <Image className="-z-10" src={backgroundSrc || ""} alt="slide" fill />
+            <Image className="-z-50" src={backgroundSrc || ""} alt="slide" fill sizes="100%" />
             {children}
         </div>
     )
