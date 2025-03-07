231a5aa5ef7891908e4d060be9ea43671dd59bcb
klivanskyr <klivanskyr@gmail.com>
2025-03-06 20:02:55 -0500

    Added ServiceAccount json for firebase admin

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -1,12 +1,27 @@
-import { applicationDefault, initializeApp, getApps } from 'firebase-admin/app';
+import { initializeApp, getApps, ServiceAccount } from 'firebase-admin/app';
+import admin from 'firebase-admin';
 import { getAuth } from 'firebase-admin/auth';
 import { cookies } from 'next/headers';
+
+const serviceAccount = {
+    type: process.env.FIREBASE_ADMIN_TYPE,
+    project_id: process.env.FIREBASE_ADMIN_PROJECT_ID,
+    private_key_id: process.env.FIREBASE_ADMIN_PRIVATE_KEY_ID,
+    private_key: process.env.FIREBASE_ADMIN_PRIVATE_KEY,
+    client_email: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
+    client_id: process.env.FIREBASE_ADMIN_CLIENT_ID,
+    auth_uri: process.env.FIREBASE_ADMIN_AUTH_URI,
+    token_uri: process.env.FIREBASE_ADMIN_TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.FIREBASE_ADMIN_AUTH_PROVIDER_X509_CERT_URL,
+    client_x509_cert_url: process.env.FIREBASE_ADMIN_CLIENT_X509_CERT_URL,
+    universe_domain: process.env.FIREBASE_ADMIN_UNIVERSE_DOMAIN
+} as ServiceAccount;
 
 let adminApp;
 
 if (!getApps().some(app => app.name === 'admin')) {
     adminApp = initializeApp({
-        credential: applicationDefault(),
+        credential: admin.credential.cert(serviceAccount),
         databaseURL: "https://book-manager-3fe0f-default-rtdb.firebaseio.com",
     }, 'admin');
 } else {
