e64d06150b5965cf02a9df163154f1a215b75bd5
klivanskyr <klivanskyr@gmail.com>
2025-04-11 19:49:50 -0400

    slide classname

--- src/components/Slider/Slide.tsx
+++ src/components/Slider/Slide.tsx
@@ -1,9 +1,7 @@
-import Image from "next/image";
-
 export default function Slide({ className="", backgroundSrc, children }: { className?: string, backgroundSrc?: string, children: React.ReactNode }) {
     return (
         <div className={`${className} relative w-full h-full`}>
-            <Image className="-z-50" src={backgroundSrc || ""} alt="slide" fill sizes="100%" />
+            <img className="w-full h-full object-fill" src={backgroundSrc || ""} />
             {children}
         </div>
     )
