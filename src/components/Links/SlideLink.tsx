c9fa4dc4596da53a76a49abfe6586e1698316dd3
klivanskyr <klivanskyr@gmail.com>
2025-04-11 19:30:09 -0400

    Image to img

--- src/components/Links/SlideLink.tsx
+++ src/components/Links/SlideLink.tsx
@@ -1,9 +1,7 @@
-import Image from "next/image";
-
 export default function SlideLink({ backgroundSrc, children }: { backgroundSrc?: string, children: React.ReactNode }) {
     return (
         <div className="group relative w-full h-full overflow-hidden transition-all">
-            <Image className="z-0 group-hover:scale-150 transition-all object-cover" src={backgroundSrc || ""} alt="slide" fill />
+            <img src={backgroundSrc || ""} alt="slide" />
             <div className="group-hover:bg-opacity-0 transition-all absolute w-full h-full bg-black bg-opacity-50 z-10"></div>
             <div className="z-20 flex flex-col w-full h-full justify-center items-center absolute top-0 left-0">
                 {children}
