8342285ec40c31cb40787743c603398c2a7fe186
klivanskyr <klivanskyr@gmail.com>
2025-04-11 15:32:50 -0400

    changing cases for hope

--- src/app/my-account/edit-account/page.tsx
+++ src/app/my-account/edit-account/page.tsx
@@ -1,4 +1,4 @@
-import { DashboardTemplate } from "@/components/DashboardHelpers";
+import { DashboardTemplate } from "@/components/dashboradHelpers";
 
 export default function Page() {
     return (
--- src/app/my-account/edit-address/page.tsx
+++ src/app/my-account/edit-address/page.tsx
@@ -1,4 +1,4 @@
-import { DashboardTemplate } from "@/components/DashboardHelpers";
+import { DashboardTemplate } from "@/components/dashboradHelpers";
 import AddressPage from "@/components/my-account/AddressPage";
 
 export default function Page() {
