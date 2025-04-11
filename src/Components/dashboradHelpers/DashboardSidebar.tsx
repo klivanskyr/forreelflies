a942de1d3db609ba8dd63eecb8e32d7649e57250
klivanskyr <klivanskyr@gmail.com>
2025-04-11 15:46:55 -0400

    commented out sidebar

--- src/Components/dashboradHelpers/DashboardSidebar.tsx
+++ src/Components/dashboradHelpers/DashboardSidebar.tsx
@@ -13,20 +13,21 @@ import { usePathname, useRouter } from "next/navigation";
 import { Suspense } from "react";
 
 export default function DashboardSidebar(): JSX.Element {
-    const router = useRouter();
-    const path = usePathname();
+    // const router = useRouter();
+    // const path = usePathname();
     
-    return (
-        <Suspense fallback={<div className="flex flex-col gap-1">Loading...</div>}>
-            <div className="flex flex-col gap-1">
-                <TextLink className={`w-fit ${path === "/my-account" ? "!text-greenPrimary" : ""}`} href="/my-account" text="Dashboard" startingIcon={<DashboardIcon className={`${path === "/my-account" ? "fill-greenPrimary" : ""}`}/>} />
-                <TextLink className={`w-fit ${path === "/my-account/orders" ? "!text-greenPrimary" : ""}`} href="/my-account/orders" text="Orders" startingIcon={<OrdersIcon className={`${path === "/my-account/orders" ? "fill-greenPrimary" : ""}`}/>} />
-                <TextLink className={`w-fit ${path === "/my-account/wishlist" ? "!text-greenPrimary" : ""}`} href="/my-account/wishlist" text="Wishlist" startingIcon={<WishlistIcon className={`${path === "/my-account/wishlist" ? "fill-greenPrimary" : ""}`}/>} />
-                <TextLink className={`w-fit ${path === "/my-account/payment-methods" ? "!text-greenPrimary" : ""}`} href="/my-account/payment-methods" text="Payment Methods" startingIcon={<PaymentIcon className={`${path === "/my-account/payment-methods" ? "fill-greenPrimary" : ""}`}/>} />
-                <TextLink className={`w-fit ${path === "/my-account/edit-account" ? "!text-greenPrimary" : ""}`} href="/my-account/edit-account" text="Account Settings" startingIcon={<SettingsIcon className={`${path === "/my-account/account-settings" ? "fill-greenPrimary" : ""}`}/>} />
-                <TextLink className={`w-fit ${path === "/my-account/edit-address" ? "!text-greenPrimary" : ""}`} href="/my-account/edit-address" text="Address Book" startingIcon={<SettingsIcon className={`${path === "/my-account/address-book" ? "fill-greenPrimary" : ""}`}/>} />
-                <SignOutButton className="w-fit" text="Sign Out" startingIcon={<LogoutIcon />} onSignOut={() => router.refresh()}/>
-            </div>
-        </Suspense>
-    )
+    // return (
+    //     <Suspense fallback={<div className="flex flex-col gap-1">Loading...</div>}>
+    //         <div className="flex flex-col gap-1">
+    //             <TextLink className={`w-fit ${path === "/my-account" ? "!text-greenPrimary" : ""}`} href="/my-account" text="Dashboard" startingIcon={<DashboardIcon className={`${path === "/my-account" ? "fill-greenPrimary" : ""}`}/>} />
+    //             <TextLink className={`w-fit ${path === "/my-account/orders" ? "!text-greenPrimary" : ""}`} href="/my-account/orders" text="Orders" startingIcon={<OrdersIcon className={`${path === "/my-account/orders" ? "fill-greenPrimary" : ""}`}/>} />
+    //             <TextLink className={`w-fit ${path === "/my-account/wishlist" ? "!text-greenPrimary" : ""}`} href="/my-account/wishlist" text="Wishlist" startingIcon={<WishlistIcon className={`${path === "/my-account/wishlist" ? "fill-greenPrimary" : ""}`}/>} />
+    //             <TextLink className={`w-fit ${path === "/my-account/payment-methods" ? "!text-greenPrimary" : ""}`} href="/my-account/payment-methods" text="Payment Methods" startingIcon={<PaymentIcon className={`${path === "/my-account/payment-methods" ? "fill-greenPrimary" : ""}`}/>} />
+    //             <TextLink className={`w-fit ${path === "/my-account/edit-account" ? "!text-greenPrimary" : ""}`} href="/my-account/edit-account" text="Account Settings" startingIcon={<SettingsIcon className={`${path === "/my-account/account-settings" ? "fill-greenPrimary" : ""}`}/>} />
+    //             <TextLink className={`w-fit ${path === "/my-account/edit-address" ? "!text-greenPrimary" : ""}`} href="/my-account/edit-address" text="Address Book" startingIcon={<SettingsIcon className={`${path === "/my-account/address-book" ? "fill-greenPrimary" : ""}`}/>} />
+    //             <SignOutButton className="w-fit" text="Sign Out" startingIcon={<LogoutIcon />} onSignOut={() => router.refresh()}/>
+    //         </div>
+    //     </Suspense>
+    return <></>;
+    // )
 }
