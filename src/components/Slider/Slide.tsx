b8fd4e7516a525e31be20e956a722ea8538267b8
klivanskyr <klivanskyr@gmail.com>
2025-04-11 20:19:51 -0400

    uncommenting code

--- src/components/Slider/Slide.tsx
+++ src/components/Slider/Slide.tsx
@@ -1,7 +1,7 @@
 export default function Slide({ className="", backgroundSrc, children }: { className?: string, backgroundSrc?: string, children: React.ReactNode }) {
     return (
         <div className={`${className} w-full h-full`}>
-            <img className="w-full h-full object-fill" src={backgroundSrc || ""} />
+            <img className="w-full h-full object-fill" draggable="false" src={backgroundSrc || ""} />
             {children}
         </div>
     )
