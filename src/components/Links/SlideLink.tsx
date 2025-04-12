25043408fa491de5bcaf732faf9485f6de4cdf7d
klivanskyr <klivanskyr@gmail.com>
2025-04-11 20:02:26 -0400

    testing slide to fix positioning

--- src/components/Links/SlideLink.tsx
+++ src/components/Links/SlideLink.tsx
@@ -1,7 +1,7 @@
 export default function SlideLink({ backgroundSrc, children }: { backgroundSrc?: string, children: React.ReactNode }) {
     return (
         <div className="group relative w-full h-full overflow-hidden transition-all">
-            <img src={backgroundSrc || ""} alt="slide" className="z-0 group-hover:scale-125 transition-all duration-300 object-cover w-full h-full" />
+            <img src={backgroundSrc || ""} alt="slide" draggable="false" className="z-0 group-hover:scale-125 transition-all duration-300 object-cover w-full h-full" />
             <div className="group-hover:bg-opacity-0 transition-all absolute w-full h-full bg-black bg-opacity-50 z-10"></div>
             <div className="z-20 flex flex-col w-full h-full justify-center items-center absolute top-0 left-0">
                 {children}
--- src/components/Slider/Slide.tsx
+++ src/components/Slider/Slide.tsx
@@ -1,6 +1,6 @@
 export default function Slide({ className="", backgroundSrc, children }: { className?: string, backgroundSrc?: string, children: React.ReactNode }) {
     return (
-        <div className={`${className} relative w-full h-full`}>
+        <div className={`${className} w-full h-full`}>
             <img className="w-full h-full object-fill" src={backgroundSrc || ""} />
             {children}
         </div>
