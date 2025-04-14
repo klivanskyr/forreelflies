c68d1b7206a87f4a05601b73f5f177395403d2fd
klivanskyr <klivanskyr@gmail.com>
2025-04-13 22:09:57 -0400

    Updated images and slider

--- src/components/Slider/Slide.tsx
+++ src/components/Slider/Slide.tsx
@@ -1,7 +1,7 @@
 export default function Slide({ className="", backgroundSrc, children }: { className?: string, backgroundSrc?: string, children: React.ReactNode }) {
     return (
         <div className={`${className} w-full h-full`}>
-            <img className="w-full h-full object-fill" draggable="false" src={backgroundSrc || ""} />
+            <img className="absolute w-full h-full object-fill -z-50" draggable="false" src={backgroundSrc || ""} />
             {children}
         </div>
     )
