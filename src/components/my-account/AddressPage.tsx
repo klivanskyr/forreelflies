f1d710ad310325ae81e2c3a905bf05ce2b08d6ce
klivanskyr <klivanskyr@gmail.com>
2025-04-14 00:48:02 -0400

    Link in cart takes to shipping addresses and updated modal

--- src/components/my-account/AddressPage.tsx
+++ src/components/my-account/AddressPage.tsx
@@ -54,24 +54,24 @@ export default function AddressPage() {
         <>
             <div className="p-4 w-full h-full flex flex-col items-center justify-center">
                 <div className="flex flex-col items-center justify-center gap-4 p-4 w-3/4">
-                    <Button className="w-full" text="Add or Update Address" onClick={() => setOpen(true)}/>
+                    <Button className="w-full text-xl" text="Add or Update Address" onClick={() => setOpen(true)}/>
                     <div className="text-lg flex flex-col">
-                        <h1>Street Address: {user.streetAddress}</h1>
-                        <h1>City: {user.city}</h1>
-                        <h1>State: {user.state}</h1>
-                        <h1>Zipcode: {user.zipCode}</h1>
-                        <h1>Country: {user.country}</h1>
+                        {user.streetAddress && <h1>Street Address: {user.streetAddress}</h1>}
+                        {user.city && <h1>City: {user.city}</h1>}
+                        {user.state && <h1>State: {user.state}</h1>}
+                        {user.zipCode && <h1>Zipcode: {user.zipCode}</h1>}
+                        {user.country && <h1>Country: {user.country}</h1>}
                     </div>
                 </div>
             </div>
-            <Modal open={open} setOpen={setOpen} >
+            <Modal className="flex flex-col items-center justify-center gap-4 w-[50dvw] h-[50dvh] rounded-md" open={open} setOpen={setOpen} >
                 <form className="flex flex-col w-[75%]" onSubmit={handleFormSubmit}>
                     <Input label="Street Address" value={input.streetAddress} onChange={(e) => setInput({ ...input, streetAddress: e.target.value })} />
                     <Input label="City" value={input.city} onChange={(e) => setInput({ ...input, city: e.target.value })} />
                     <Input label="State" value={input.state} onChange={(e) => setInput({ ...input, state: e.target.value })} />
                     <Input label="Zip Code" value={input.zipCode} onChange={(e) => setInput({ ...input, zipCode: e.target.value })} />
                     <Input label="Country" value={input.country} onChange={(e) => setInput({ ...input, country: e.target.value })} />
-                    <Button text="Save" type="submit" />
+                    <Button className="mt-1" text="Save" type="submit" />
                 </form>
             </Modal>
         </>
