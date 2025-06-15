8a68b3f8a019f24f3043ce8fa6ab6f5ee8676c8f
klivanskyr <klivanskyr@gmail.com>
2025-06-15 12:49:19 -0400

    create-connect-account

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 1. Prices shouldn't be numbers in jsons because a user could change them.
 2. clicking checkout without shipping address raises error
 3. Adding address should instantly update page 
+4. Validate and ValidateToken should be one call
+5. Click here to become a vendor is misspelt and does nothing when not signed it.
--- src/app/vendor-signup/page.tsx
+++ src/app/vendor-signup/page.tsx
@@ -1,82 +1,126 @@
 'use client';
 
-import VendorSignupForm from "@/components/vendorSignupHelpers/VendorSignupForm";
+import Button from "@/components/buttons/Button";
+import NoXRedirect from "@/components/NoXRedirect";
 import { useUser } from "@/contexts/UserContext";
-import { DbUser } from "@/lib/firebase-admin";
-import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
-
-type ApprovalStatus = "approved" | "pending" | "not-submitted";
 
 export default function Page() {
-    const [status, setStatus] = useState<ApprovalStatus>("not-submitted");
     const { user } = useUser();
-    const router = useRouter();
 
-    useEffect(() => {
-        const getApprovalStatus = async (user: DbUser): Promise<ApprovalStatus> => {
-            const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/vendor/approve-vendor?uid=${user.uid}`, {
-                method: "GET",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-            });
-    
-            if (response.status === 200) {
-                const data = await response.json();
-    
-                if (data?.vendorRequest?.isApproved) {
-                    console.log("approved");
-                    return "approved";
-                } else {
-                    console.log("pending");
-                    return "pending";
-                }
-            } else {
-                console.log("not-submitted");
-                return "not-submitted";
-            }
-        }
-
-        if (!user) return
-
-        getApprovalStatus(user).then((status) => {
-            setStatus(status);
+    const handleStripeSignUp = async () => {
+        const createAccountResponse = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/stripe/create-connect-account`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ uid: user?.uid })
         });
 
-    }, [user]);
+        if (!createAccountResponse.ok) {
+            console.error("Failed to create Stripe account");
+            return;
+        }
 
-    if (!user) {
-        router.push("/?login=true");
-        return <></>;
+        const accountLinkData = await createAccountResponse.json();
+        const onboardingLink = accountLinkData.onboardingLink;
     }
 
-    switch (status) {
-        case "approved":
-            return (
-                <div className="flex flex-col w-full h-[60vh] items-center justify-center">
-                    <div className="bg-greenPrimary p-8 text-center flex flex-col items-center justify-center">
-                        <h1 className="text-white text-2xl mb-4">Your Request Was Approved!</h1>
-                        <h2 className="text-sm text-white">You can now start selling your products on For Reel Flies.</h2>
-                        <h2 className="text-sm text-white font-bold">You can access the store-manager by clicking on your avatar in the top right.</h2>
-                        <h3 className="text-sm text-white">Contact us at help@forreelflies.com if there is a problem.</h3>
-                    </div>
-                </div>
-            )
-        case "pending":
-            return (
-                <div className="flex flex-col w-full h-[60vh] items-center justify-center">
-                    <div className="bg-greenPrimary p-8 text-center flex flex-col items-center justify-center">
-                        <h1 className="text-white text-2xl mb-4">Your Request Is Currently Pending</h1>
-                        <h2 className="text-sm text-white">You request will soon be processed.</h2>
-                        <h2 className="text-sm text-white">Refresh the page if you believe the status has change.</h2>
-                        <h3 className="text-sm text-white">Contact us at help@forreelflies.com if there is a problem.</h3>
-                    </div>
-                </div>
-            )
-        case "not-submitted":
-            return (
-                <VendorSignupForm uid={user?.uid || null} />
-            )
-    }
-}
+    return (
+        <NoXRedirect x={user} redirectUrl="/?login=true">
+            <div>
+                <h1>Sign up through stripe to become a verified member</h1>
+                <Button text="Sign Up Through Stripe" onClick={handleStripeSignUp}/>
+            </div>
+        </NoXRedirect>
+    )
+}   
+    
+    
+    
+    
+    
+    
+    
+    
+// 'use client';
+
+// import VendorSignupForm from "@/components/vendorSignupHelpers/VendorSignupForm";
+// import { useUser } from "@/contexts/UserContext";
+// import { DbUser } from "@/lib/firebase-admin";
+// import { useRouter } from "next/navigation";
+// import { useEffect, useState } from "react";
+
+// type ApprovalStatus = "approved" | "pending" | "not-submitted";
+
+// export default function Page() {
+//     const [status, setStatus] = useState<ApprovalStatus>("not-submitted");
+//     const { user } = useUser();
+//     const router = useRouter();
+
+//     useEffect(() => {
+//         const getApprovalStatus = async (user: DbUser): Promise<ApprovalStatus> => {
+//             const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/vendor/approve-vendor?uid=${user.uid}`, {
+//                 method: "GET",
+//                 headers: {
+//                     "Content-Type": "application/json"
+//                 },
+//             });
+    
+//             if (response.status === 200) {
+//                 const data = await response.json();
+    
+//                 if (data?.vendorRequest?.isApproved) {
+//                     console.log("approved");
+//                     return "approved";
+//                 } else {
+//                     console.log("pending");
+//                     return "pending";
+//                 }
+//             } else {
+//                 console.log("not-submitted");
+//                 return "not-submitted";
+//             }
+//         }
+
+//         if (!user) return
+
+//         getApprovalStatus(user).then((status) => {
+//             setStatus(status);
+//         });
+
+//     }, [user]);
+
+//     if (!user) {
+//         router.push("/?login=true");
+//         return <></>;
+//     }
+
+//     switch (status) {
+//         case "approved":
+//             return (
+//                 <div className="flex flex-col w-full h-[60vh] items-center justify-center">
+//                     <div className="bg-greenPrimary p-8 text-center flex flex-col items-center justify-center">
+//                         <h1 className="text-white text-2xl mb-4">Your Request Was Approved!</h1>
+//                         <h2 className="text-sm text-white">You can now start selling your products on For Reel Flies.</h2>
+//                         <h2 className="text-sm text-white font-bold">You can access the store-manager by clicking on your avatar in the top right.</h2>
+//                         <h3 className="text-sm text-white">Contact us at help@forreelflies.com if there is a problem.</h3>
+//                     </div>
+//                 </div>
+//             )
+//         case "pending":
+//             return (
+//                 <div className="flex flex-col w-full h-[60vh] items-center justify-center">
+//                     <div className="bg-greenPrimary p-8 text-center flex flex-col items-center justify-center">
+//                         <h1 className="text-white text-2xl mb-4">Your Request Is Currently Pending</h1>
+//                         <h2 className="text-sm text-white">You request will soon be processed.</h2>
+//                         <h2 className="text-sm text-white">Refresh the page if you believe the status has change.</h2>
+//                         <h3 className="text-sm text-white">Contact us at help@forreelflies.com if there is a problem.</h3>
+//                     </div>
+//                 </div>
+//             )
+//         case "not-submitted":
+//             return (
+//                 <VendorSignupForm uid={user?.uid || null} />
+//             )
+//     }
+// }
